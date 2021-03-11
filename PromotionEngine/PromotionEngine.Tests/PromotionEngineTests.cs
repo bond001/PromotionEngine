95f93a98551e976ee8e4f25e10a938337a6a0835
vamsisiva007 <vamsisiva007@gmail.com>
2021-03-12 01:38:03 +0530

    Added test for checking if total order of 2items of B equals 45

--- PromotionEngine/PromotionEngine.Tests/PromotionEngineTests.cs
+++ PromotionEngine/PromotionEngine.Tests/PromotionEngineTests.cs
@@ -12,5 +12,13 @@ namespace PromotionEngine.Tests
             int totalOrder = promotionEngine.ApplyPromotion(new Item[] { new Item("A", 50, 3)} );
             Assert.Equal(130, totalOrder);
         }
+
+        [Fact]
+        public void Should_Return_45_For_Two_Items_Of_B()
+        {
+            PromotionEngine promotionEngine = new PromotionEngine();
+            int totalOrder = promotionEngine.ApplyPromotion(new Item[] { new Item("B", 30, 2) });
+            Assert.Equal(45, totalOrder);
+        }
     }
 }
