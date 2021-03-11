8b9716d834650364a286b1921ced704f1aff90b3
vamsisiva007 <vamsisiva007@gmail.com>
2021-03-12 01:54:17 +0530

    Added test for totalorder to equal 100 for one item each of A, B and C

--- PromotionEngine/PromotionEngine.Tests/PromotionEngineTests.cs
+++ PromotionEngine/PromotionEngine.Tests/PromotionEngineTests.cs
@@ -20,5 +20,13 @@ namespace PromotionEngine.Tests
             int totalOrder = promotionEngine.ApplyPromotion(new Item[] { new Item("B", 30, 2) });
             Assert.Equal(45, totalOrder);
         }
+
+        [Fact]
+        public void Should_Return_100_For_One_Item_Each_Of_A_B_C()
+        {
+            PromotionEngine promotionEngine = new PromotionEngine();
+            int totalOrder = promotionEngine.ApplyPromotion(new Item[] { new Item("A", 50, 1), new Item("B", 30, 1), new Item("C", 20, 1) });
+            Assert.Equal(100, totalOrder);
+        }
     }
 }
