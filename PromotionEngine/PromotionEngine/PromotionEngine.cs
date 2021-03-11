ccedb9b3b46bc919f15980341ba328e9d4e55004
vamsisiva007 <vamsisiva007@gmail.com>
2021-03-12 01:18:56 +0530

    added promotion engine class to pass the basic test for total order of 3 items of A equals 130

---
+++ PromotionEngine/PromotionEngine/PromotionEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class PromotionEngine
+    {
+        public int ApplyPromotion(char[] skus, int[] skuQuantity)
+        {
+            if (skus[0] == 'A' && skuQuantity[0] == 3)
+            {
+                return 130;
+            }
+            return 0;
+        }
+    }
+}
